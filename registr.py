af27244ba4b6a7a7a4344bce5aa927f538216023
b-saidazim-b <saidazimbakhodirof@gmail.com>
2021-10-13 07:11:23 +0500

    2.mysql ulandi register ham tugap qoldi

--- registr.py
+++ registr.py
@@ -1,5 +1,13 @@
 import os
+import mysql.connector.connection
 
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='a1',
+    passwd='12345678',
+    database='user'
+)
+mycursor = mydb.cursor()
 
 class User:
     def __init__(self, name, login, password, age, gender):
@@ -9,6 +17,7 @@ class User:
         self.age = age
         self.gender = gender
         self.option = ['0', '1', '2']
+        self.gen_option = ['0', '1']
 
     def show_menu(self):
         print('''
@@ -27,12 +36,68 @@ class User:
             print('Invalid syntax!')
             input_user = input('[0, 1, 2]: ').strip()
 
+        if input_user == self.option[0]:
+            self.exitt()
+
+        if input_user == self.option[1]:
+            self.register()
+
+        else:
+            self.log_in()
+
     def exitt(self):
         print('Good bye!')
         exit()
 
     def register(self):
-        pass
+        input_name = input('Name: ').strip().capitalize()
+        while self.empty(input_name):
+            self.clear()
+            print("It's empty string please again")
+            input_name = input('Name: ').strip().capitalize()
+
+        input_login = input('Login: ').strip().lower()
+        while self.empty(input_login):
+            self.clear()
+            print('Name: ', input_name)
+            print("It's empty string please again")
+            input_login = input('Login: ').strip().lower()
+
+        input_password = input('Password: ').strip()
+        while self.empty(input_password):
+            self.clear()
+            print('Name: ', input_name)
+            print('Login', input_login)
+            print("It's empty string please again")
+            input_password = input('Password: ').strip()
+
+        input_age = input('Age: ').strip()
+        while self.empty(input_age) or not input_age.isdigit():
+            self.clear()
+            print('Name: ', input_name)
+            print('Password: ', input_password)
+            print('Login', input_login)
+            print("It's empty string please again")
+            input_age = input('Age: ').strip()
+
+        input_gender = input('Gender: ').strip()
+        while self.empty(input_gender) or input_gender not in self.gen_option:
+            self.clear()
+            print('Name: ', input_name)
+            print('Password: ', input_password)
+            print('Age: ', input_age)
+            print('Login', input_login)
+            print("""It's empty string please again
+[1] - > male 
+[0] - > female """)
+            input_gender = input('Gender: ').strip()
+
+        sql = 'insert into user (name, login, password, age, gender) values (%s, %s, %s, %s, %s)'
+        val = input_name, input_login, input_password, input_age, input_gender
+        mycursor.execute(sql, val)
+
+        print("\nYou've successfully registered)")
+        mydb.commit()
 
     def log_in(self):
         pass
@@ -41,6 +106,9 @@ class User:
     def clear():
         os.system('clear')
 
+    @staticmethod
+    def empty(a):
+        return not a
 
 dilshod = User('dilishod', 'shamshod', 'dili', 24, 1)
 dilshod.enter_sys()
